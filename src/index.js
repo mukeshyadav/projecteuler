c7125d19c5692b221e9c6725c52be59eed5e38f6
Mukesh Yadav <mukeshyadav@gmail.com>
2019-12-24 21:35:12 +0530

    evenFibonacciNumbers
    sum of even fibonacci numbers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import "./styles.css";
+//sum of even number in fibonacci series
+//1, 2, 3, 5, 8, 13, 21, 34, 55 => 44
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function fibonacci(num) {
+  let fibo = [0, 1];
+  let evenFiboSum = 0;
+  for (let i = 2; i < num; i++) {
+    let sfibo = fibo[i - 2] + fibo[i - 1];
+    fibo[i] = sfibo;
+    if (sfibo < num && sfibo % 2 === 0) {
+      evenFiboSum += sfibo;
+    }
+  }
+  return evenFiboSum;
+}
+
+console.log(fibonacci(4000000));
